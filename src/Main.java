7b846b08e94ae81976450b8390ddb0205a880265
prost333 <dimabolshoy333@yandex.by>
2023-03-13 13:07:50 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,50 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+
+
+//        task1
+//        if (a%2==0){
+//            System.out.println(a);
+//        } if (b%2==0){
+//            System.out.println(b);
+//        } if (c%2==0){
+//            System.out.println(c);
+//        }
+//        else;
+
+//        task2
+//           if (a%2==0 && a%5==0){
+//               System.out.println(a);
+//           }
+//           if  (b%2==0 && b%5==0){
+//               System.out.println(b);
+//           }
+//           if  (c%2==0 && c%5==0){
+//               System.out.println(c);
+//           }
+//                else;
+
+//        task3
+//        int result ;
+//          switch (result=15/a){
+//              case 3 :
+//                  if (a==5){
+//                  System.out.println("Результат деления 3");}
+//                  else {System.out.println("результат деления равен дробному числу: "+ (float)15/a);}
+//                  break;
+//              case 5 :
+//                  System.out.println("Результат деления 5");
+//                  break;
+//              default:
+//                  System.out.println("результат деления равен дробному числу: "+ (float)15/a);
+//          }
+
+
+    }
+}
